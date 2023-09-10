c9978e8d88b1ff5c6fe2b41f222658032928403d
Charlier Benjamin <benjamin.charlier@umontpellier.fr>
2023-09-10 22:18:14 +0200

    Sufix every variables and file names with non_gaussian. Will help when merging.

--- generate_data.py
+++ generate_data.py
@@ -3,7 +3,7 @@ import numpy as np
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
-mu, sigma = 100, 5
-x = mu + sigma * (np.random.randn(10,10000) ** 2).sum(0)
+mu_non_gaussian, sigma_non_gaussian = 100, 5
+x_non_gaussian = mu_non_gaussian + sigma_non_gaussian * (np.random.randn(10,10000) ** 2).sum(0)
 
-np.savetxt('file_data.csv', x, delimiter=',')
+np.savetxt('file_data_non_gaussian.csv', x_non_gaussian, delimiter=',')
--- plot.py
+++ plot.py
@@ -1,16 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.loadtxt('file_data.csv')
+x_non_gaussian = np.loadtxt('file_data_non_gaussian.csv')
 
 # the histogram of the data
-n, bins, patches = plt.hist(x, 50, density=True, facecolor='k', alpha=0.25)
+n, bins, patches = plt.hist(x_non_gaussian, 50, density=True, facecolor='k', alpha=0.25)
 
 
 plt.xlabel('Smarts')
 plt.ylabel('Probability')
 plt.title('Histogram of IQ')
-plt.text(60, .025, r'$\mu=100,\ \sigma=5$ but this is no longer a Gaussian')
+plt.text(115, .020, r'$\mu=100,\ \sigma=5$ but this is no longer a Gaussian')
 plt.xlim(40, 250)
 plt.ylim(0, 0.03)
 plt.grid(True)
